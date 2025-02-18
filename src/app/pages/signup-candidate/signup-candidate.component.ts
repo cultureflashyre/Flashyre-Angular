709d571ffea29423d3c2a597aa616b2700d8eeff
Prashanth <prashanth6591@yahoo.com>
2025-02-18 16:24:27 +0530

    Candidate Signup page

--- src/app/pages/signup-candidate/signup-candidate.component.ts
+++ src/app/pages/signup-candidate/signup-candidate.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core'
 import { Title, Meta } from '@angular/platform-browser'
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'signup-candidate',
@@ -7,7 +8,15 @@ import { Title, Meta } from '@angular/platform-browser'
   styleUrls: ['signup-candidate.component.css'],
 })
 export class SignupCandidate {
-  constructor(private title: Title, private meta: Meta) {
+  firstName: string = '';
+  lastName: string = '';
+  phoneNumber: string = '';
+  email: string = '';
+  password: string = '';
+  confirmPassword: string = '';
+  message: string = '';
+
+  constructor(private title: Title, private meta: Meta,private http: HttpClient) {
     this.title.setTitle('Signup-Candidate - Flashyre')
     this.meta.addTags([
       {
@@ -20,5 +29,28 @@ export class SignupCandidate {
           'https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/8203932d-6f2d-4493-a7b2-7000ee521aa2/9aea8e9c-27ce-4011-a345-94a92ae2dbf8?org_if_sml=1&force_format=original',
       },
     ])
+  }
+  onSubmit() {
+    const data = {
+      first_name: this.firstName,
+      last_name: this.lastName,
+      phone_number: this.phoneNumber,
+      email: this.email,
+      password: this.password
+    };
+
+    this.message = 'The data is being sent...';
+
+    this.http.post('http://localhost:8000/api/signup-candidate/', data)
+      .subscribe(
+        (response: any) => {
+          this.message = 'Signup successful!';
+          console.log(response);
+        },
+        (error) => {
+          this.message = 'Error during signup. Please try again.';
+          console.error(error);
+        }
+      );
   }
 }
