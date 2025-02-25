0d39dd879a714121e52e36f514b204d87bca9760
Prashanth <prashanth6591@yahoo.com>
2025-02-25 11:11:21 +0530

    Corporate Login

--- src/app/pages/login-corporate/login-corporate.module.ts
+++ src/app/pages/login-corporate/login-corporate.module.ts
@@ -1,16 +1,15 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
-import { RouterModule } from '@angular/router'
-import { CommonModule } from '@angular/common'
-
-import { ComponentsModule } from '../../components/components.module'
-import { LoginCorporate } from './login-corporate.component'
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ComponentsModule } from '../../components/components.module';
+import { LoginCorporate } from './login-corporate.component';
 
 const routes = [
   {
     path: '',
     component: LoginCorporate,
   },
-]
+];
 
 @NgModule({
   declarations: [LoginCorporate],
