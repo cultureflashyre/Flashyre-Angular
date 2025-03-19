0a4045affcf579e38ac467154331b53d3352c036
Abhishek <abhishekmk908@gmail.com>
2025-03-19 22:55:01 +0530

    Updated the hamburger menu for landing navbar and candidate navbar

--- src/app/components/landing-page-navbar/landing-page-navbar.component.ts
+++ src/app/components/landing-page-navbar/landing-page-navbar.component.ts
@@ -8,35 +8,59 @@ import { Component, Input, ContentChild, TemplateRef } from '@angular/core'
 export class LandingPageNavbar {
   @ContentChild('text2')
   text2: TemplateRef<any>
+
   @Input()
   imageAlt1: string = 'image'
+
   @ContentChild('link4')
   link4: TemplateRef<any>
+
   @ContentChild('text3')
   text3: TemplateRef<any>
+
   @ContentChild('text9')
   text9: TemplateRef<any>
+
   @ContentChild('text13')
   text13: TemplateRef<any>
+
   @Input()
   link4Url: string = 'https://www.teleporthq.io'
+
   @Input()
   rootClassName: string = ''
+
   @ContentChild('text')
   text: TemplateRef<any>
+
   @ContentChild('text10')
   text10: TemplateRef<any>
+
   @ContentChild('text1')
   text1: TemplateRef<any>
+
   @Input()
   link3Url: string = 'https://www.teleporthq.io'
+
   @ContentChild('text12')
   text12: TemplateRef<any>
+
   @ContentChild('text11')
   text11: TemplateRef<any>
+
   @Input()
   imageSrc1: string = '/assets/main-logo/logo%20-%20flashyre(1500px)-200h.png'
+
   @ContentChild('link3')
   link3: TemplateRef<any>
+
+  // Property to track mobile menu state
+  isMobileMenuOpen: boolean = false;
+
   constructor() {}
+
+  // Method to toggle mobile menu visibility
+  toggleMobileMenu(): void {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
 }
