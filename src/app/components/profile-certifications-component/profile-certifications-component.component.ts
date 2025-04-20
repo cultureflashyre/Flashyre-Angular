ed848caae91f1192785330038b03bcb537a5c854
vignesh_br <vigneshbr12@gmail.com>
2025-04-20 12:48:25 +0530

    murges from all colleagues branches

--- src/app/components/profile-certifications-component/profile-certifications-component.component.ts
+++ src/app/components/profile-certifications-component/profile-certifications-component.component.ts
@@ -1,36 +1,27 @@
-import { Component, Input, ContentChild, TemplateRef, Output, EventEmitter } from '@angular/core';
+import { Component, Input, ContentChild, TemplateRef, OnInit, ViewChild, ViewChildren, QueryList, ElementRef } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-
 import { NgxSpinnerService } from 'ngx-spinner'; // Import NgxSpinnerService
-import { environment } from '../../../environments/environment';
-import { CertificationService } from 'src/app/services/certification.service';
-import { forkJoin } from 'rxjs';
-
 
 @Component({
   selector: 'profile-certifications-component',
-  templateUrl: './profile-certifications-component.component.html',
-  styleUrls: ['./profile-certifications-component.component.css'],
+  templateUrl: 'profile-certifications-component.component.html',
+  styleUrls: ['profile-certifications-component.component.css'],
 })
-
 export class ProfileCertificationsComponent implements OnInit {
-  private baseUrl = environment.apiUrl;
-
-
   @ContentChild('text1') text1: TemplateRef<any>;
   @ContentChild('text312') text312: TemplateRef<any>;
-  @ContentChild('text12') text12: TemplateRef<any>;
   @ContentChild('text1111') text1111: TemplateRef<any>;
   @ContentChild('text311') text311: TemplateRef<any>;
   @ContentChild('text1121') text1121: TemplateRef<any>;
   @ContentChild('text3121') text3121: TemplateRef<any>;
   @ContentChild('text11') text11: TemplateRef<any>;
+  @Input() rootClassName: string = '';
   @ContentChild('text112') text112: TemplateRef<any>;
   @ContentChild('text111') text111: TemplateRef<any>;
   @ContentChild('text') text: TemplateRef<any>;
   @ContentChild('text31') text31: TemplateRef<any>;
   @ContentChild('text3111') text3111: TemplateRef<any>;
-
 
   @ViewChild('scrollContainer', { static: false }) scrollContainer!: ElementRef;
   @ViewChildren('certificationBlock') certificationBlocks!: QueryList<ElementRef>;
@@ -39,100 +30,105 @@ export class ProfileCertificationsComponent implements OnInit {
   todayDate: string;
 
   constructor(private fb: FormBuilder, 
-    private http: HttpClient, private spinner: NgxSpinnerService,
-    private certificationService: CertificationService,
+    private http: HttpClient, private spinner: NgxSpinnerService
   ) {
     this.certificationForm = this.fb.group({
       certifications: this.fb.array([this.createCertificationGroup()]),
     });
   }
 
+  ngOnInit() {
+    const today = new Date();
+    this.todayDate = today.toISOString().split('T')[0];
+  }
 
-  certifications: Certification[] = [
-    {
-      certificate_name: '',
-      issuing_institute: '',
-      issued_date: '',
-      renewal_date: '',
-      credentials: ''
-    }
-  ];
+  get certifications() {
+    return this.certificationForm.get('certifications') as FormArray;
+  }
 
-  private apiUrl = 'http://localhost:8000/api/certifications/';
-
-  constructor(private http: HttpClient) {}
+  createCertificationGroup(): FormGroup {
+    return this.fb.group({
+      certificate_name: ['', Validators.required],
+      issuing_institute: ['', Validators.required],
+      issued_date: ['', Validators.required],
+      renewal_date: [''],
+      credentials: ['', Validators.required],
+    });
+  }
 
   addCertification() {
-    this.certifications.push({
-      certificate_name: '',
-      issuing_institute: '',
-      issued_date: '',
-      renewal_date: '',
-      credentials: ''
-    });
-    this.emitData();
+    this.certifications.push(this.createCertificationGroup());
+    // Wait for the DOM to update, then scroll to the last block within the container
+    setTimeout(() => {
+      this.scrollToLastCertification();
+    }, 0);
   }
 
   removeCertification(index: number) {
     if (this.certifications.length > 1) {
-      this.certifications.splice(index, 1);
-      this.emitData();
+      this.certifications.removeAt(index);
     }
   }
 
-  onFieldChange(index: number) {
-    this.emitData();
+  scrollToLastCertification() {
+    const blocks = this.certificationBlocks.toArray();
+    if (blocks.length > 0 && this.scrollContainer) {
+      const lastBlock = blocks[blocks.length - 1].nativeElement;
+      const container = this.scrollContainer.nativeElement;
+      const blockOffsetTop = lastBlock.offsetTop - container.offsetTop;
+      container.scrollTo({
+        top: blockOffsetTop,
+        behavior: 'smooth'
+      });
+    }
   }
 
-  onIssuedDateChange(index: number) {
-    const cert = this.certifications[index];
-    if (cert.issued_date) {
-      const issuedDate = new Date(cert.issued_date);
-      const today = new Date();
-      if (issuedDate > today) {
-        cert.issued_date = this.getTodayDate();
-      }
-      if (cert.renewal_date && new Date(cert.renewal_date) <= issuedDate) {
-        cert.renewal_date = '';
+  updateRenewalDateMin(index: number) {
+    const certification = this.certifications.at(index) as FormGroup;
+    const issuedDate = certification.get('issued_date')?.value;
+    const renewalDateControl = certification.get('renewal_date');
+
+    if (issuedDate) {
+      const renewalDate = renewalDateControl?.value;
+      const today = new Date(this.todayDate);
+      const issued = new Date(issuedDate);
+      const renewal = renewalDate ? new Date(renewalDate) : null;
+      if (renewal && (renewal < issued || renewal > today)) {
+        renewalDateControl?.setValue('');
       }
     }
-    this.emitData();
   }
 
   async submitCertification(): Promise<void> {
     if (this.certificationForm.valid) {
       const data = this.certificationForm.value.certifications;
-      const requests = data.map((cert: any) => 
-        this.certificationService.saveCertification(cert)
+      const promises = data.map((cert: any) =>
+        this.http.post('http://localhost:8000/api/certifications/', cert, { withCredentials: true }).toPromise()
+          .then((response) => {
+            console.log('Certification saved:', response);
+          })
+          .catch((error) => {
+            console.error('Error saving certification:', error);
+            throw error;
+          })
       );
 
-  getMinRenewalDate(index: number): string {
-    const cert = this.certifications[index];
-    if (cert.issued_date) {
-      const issuedDate = new Date(cert.issued_date);
-      issuedDate.setDate(issuedDate.getDate() + 1);
-      return issuedDate.toISOString().split('T')[0];
-    }
-    return this.getTodayDate();
-  }
-
-  emitData() {
-    this.certificationData.emit(this.certifications);
-  }
+      // Show spinner before making requests
+      this.spinner.show();
 
       try {
-        await forkJoin(requests).toPromise(); // Wait for all requests to complete
+        await Promise.all(promises);
         this.certificationForm.reset();
         this.certifications.clear();
         this.certifications.push(this.createCertificationGroup());
-        console.log('All certifications saved successfully');
       } catch (error) {
-        console.error('Error saving certifications:', error);
-        // Handle error accordingly, e.g., show a notification to the user
+        // Error already logged in catch block above
       } finally {
         // Hide spinner after all requests are completed
         this.spinner.hide();
       }
-    });
+    } else {
+      console.log('Form is invalid');
+    }
   }
 }
