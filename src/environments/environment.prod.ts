b1bab4aa22b0d4c5e56a8316961a2d1c06ba1660
Prashanth <prashanth6591@yahoo.com>
2025-09-22 15:58:04 +0530

    Modified environments.prod to use new URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 
 export const environment = {
   production: true,
-    apiUrl: 'https://backend-dot-flashyre-testing.el.r.appspot.com/'
+    apiUrl: 'https://backend-dot-flashyre-staging-464607.el.r.appspot.com/'
 };
