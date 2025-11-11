7e95ca70ba378a050e1d7409e0905cf74c33ee45
vignesh_br <vigneshbr12@gmail.com>
2025-11-11 15:11:33 +0530

    update_n

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,12 +4,12 @@ export const environment = {
   apiUrl: '',
   websocketUrl: '',
 
-  defaultProfilePicture: 'https://storage.googleapis.com/flashyre-staging-b1/default_images/profile-placeholder.jpg',
-  defaultCompanyIcon: 'https://storage.googleapis.com/flashyre-staging-b1/default_images/company_icon.png',
+  defaultProfilePicture: 'https://storage.googleapis.com/flashyre-prod-b1/default_images/profile-placeholder.jpg',
+  defaultCompanyIcon: 'https://storage.googleapis.com/flashyre-prod-b1/default_images/defaultCompanyIcon.png',
 
-  fh_logo_thumbnail: 'https://storage.googleapis.com/flashyre-staging-b1/default_images/fh_logo_thumbnail.png',
-  chcs_logo_thumbnail: 'https://storage.googleapis.com/flashyre-staging-b1/default_images/chcs_logo_thumbnail.jfif',
+  fh_logo_thumbnail: 'https://storage.googleapis.com/flashyre-prod-b1/default_images/fh_logo_thumbnail.png',
+  chcs_logo_thumbnail: 'https://storage.googleapis.com/flashyre-prod-b1/default_images/chcs_logo_thumbnail.jfif',
 
-  mcq_upload_template: 'https://storage.googleapis.com/cflashyre-dev-b2/mcq_question_upload_template/flashyre_mcq_questions_template.xlsx',
+  mcq_upload_template: 'https://storage.googleapis.com/flashyre-prod-b1/mcq_question_upload_template/flashyre_mcq_questions_template.xlsx',
   googleMapsApiKey: ''
 };
