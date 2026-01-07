6fef83ffa35a3816667fe2e2cfd72fc53a399cd0
Prashanth <prashanth6591@yahoo.com>
2026-01-07 10:44:08 +0530

    Updated the google client id key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -16,7 +16,7 @@ export const environment = {
 
   mcq_upload_template: 'https://storage.googleapis.com/cflashyre-dev-b2/mcq_question_upload_template/flashyre_mcq_questions_template.xlsx',
   googleMapsApiKey: 'AIzaSyBu19cFmRm59qhFwr3VlLGxpgtaGrG3d0Q',
-  googleClientId: '76855691155-qj1bb6i1fcidraejoicasbpe1hi6g283.apps.googleusercontent.com'
+  googleClientId: '970909670920-6r0aibmfor13ti355uh8avfqd4nsaiqf.apps.googleusercontent.com',
 };
 
 /*
